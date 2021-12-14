dac80807b450f239055d53ad18bd58a76868cc4c
yuhsin.chen <s63232@gmail.com>
2021-12-14 09:42:09 +0800

    feat: Import antd for UI components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Card } from "antd";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="wrapper">
+        <Card title="Card title" bordered={false} style={{ width: 300 }}>
+          <p>Card content</p>
+          <p>Card content</p>
+          <p>Card content</p>
+        </Card>
+        <Button type="primary">Button</Button>
+      </section>
     </div>
   );
 }
